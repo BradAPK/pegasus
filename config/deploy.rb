ea6b12c640609a7346279135cda4029745d3f737
Bradley Allen Pollard-Krigbaum <Bradap42@gmail.com>
2017-12-16 17:50:52 -0700

    switch database to postgres

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock "~> 3.10.1"
 
 set :application, "our_memories"
-set :repo_url, "git@github.com/BradAPK/pegasus.git"
+set :repo_url, "https://github.com/BradAPK/pegasus.git"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
